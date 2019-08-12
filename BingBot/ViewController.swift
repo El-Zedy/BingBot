204b42933fcb3e765d35ce24848f3f3dd1752718
Muhammad El Zedy <mohamedelzedyfcih@gmail.com>
2019-08-12 13:24:40 +0200

    Initial Commit

---
+++ BingBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingBot
+//
+//  Created by Muhammad El Zedy on 8/12/19.
+//  Copyright © 2019 ZozShark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
