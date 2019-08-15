f542803b96c6888a59834b51384cd386f39cf2aa
Muhammad El Zedy <mohamedelzedyfcih@gmail.com>
2019-08-15 15:46:02 +0200

    BingBot App

--- BingBot/ViewController.swift
+++ BingBot/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var showsLabel: UILabel!
+    @IBOutlet weak var randomShowLabel: UILabel!
+    @IBOutlet weak var addShowTextField: UITextField!
+    @IBOutlet weak var addShowButton: UIButton!
+    @IBOutlet weak var bingbotSpokenLabel: UILabel!
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    @IBOutlet weak var addShowStackView: UIStackView!
+    @IBOutlet weak var ShowsStackView: UIStackView!
+    
+    var shows: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ShowsStackView.isHidden = true
+        randomShowStackView.isHidden = true
     }
 
-
+    //MARK: - Get Random Of Shows
+    @IBAction func randomBingBtnWasPressed(_ sender: Any) {
+        randomShowLabel.text = shows.randomElement()
+        randomShowLabel.isHidden = false
+        bingbotSpokenLabel.isHidden = false 
+    }
+    
+    //MARK: - Update ShowLabel With shows you Entered In the Array
+    func updateShowLabel(){
+        showsLabel.text = shows.joined(separator: ", ")
+    }
+    
+    //MARK: - Add show Btn Was Pressed To add Show in The Shows Array
+    @IBAction func addShowBtnWasPressed(_ sender: Any) {
+        guard let showName = addShowTextField.text else{return}
+        shows.append(showName)
+        updateShowLabel()
+        addShowTextField.text = ""
+        ShowsStackView.isHidden = false
+        
+        if shows.count > 1 {
+            randomShowStackView.isHidden = false
+            randomShowLabel.isHidden = true
+            bingbotSpokenLabel.isHidden = true
+        }
+        
+        addShowTextField.resignFirstResponder()
+    }
+    
 }
 
